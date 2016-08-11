38c333330c1345ff31641f586dd62644fa2e7528
chriswang1990 <1990chriswang1990@gmail.com>
2016-08-11 15:38:34 -0700

    initial commit for C++ practice

---
+++ helloworld.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	cout << "Hello World" << endl;
+
+	const double PI = 3.1415926545;
+
+	char myGrade = 'A';
+
+	bool isHappy = true;
+
+	int myAge = 26;
+
+	float favNum = 3.141592;
+
+	double otherFavNum = 1.6180339887;
+
+	cout << "favorite Number is " << favNum << endl;
+
+	return 0;
+}
